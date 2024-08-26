2b52f643984463ea4dfe26643d667ba4ade10018
MatheusKpv <matheusmatias1910@gmail.com>
2024-08-26 15:55:18 -0300

    finalizando atividade aula 1

--- src/main/java/jv/triersistemas/atividades/controller/TarefaController.java
+++ src/main/java/jv/triersistemas/atividades/controller/TarefaController.java
@@ -1,19 +1,14 @@
 package jv.triersistemas.atividades.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import jv.triersistemas.atividades.model.Tarefa;
 import jv.triersistemas.atividades.model.TarefaDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("tarefas")
@@ -22,14 +17,58 @@ public class TarefaController {
 	private Long cont = 0L;
 	
 	@GetMapping("/{id}")
-	public Tarefa getListaTarefas(@PathVariable Long id) {
-		return tarefas.forEach(c -> c.ge)
+	public ResponseEntity<Tarefa> getTarefa(@PathVariable Long id) {
+		Optional<Tarefa> opTarefa =  tarefas.stream().filter(t -> t.getId().equals(id)).findFirst();
+        return opTarefa.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+//		OU
+//		if (opTarefa.isPresent()) {
+//			return ResponseEntity.ok(opTarefa.get());
+//		}
+//		return ResponseEntity.notFound().build();
+    }
+
+	@GetMapping
+	public ResponseEntity<List<Tarefa>> getListaTarefas() {
+		return ResponseEntity.ok(tarefas);
 	}
 	
 	@PostMapping
-	public void cadastraTarefa(@RequestBody TarefaDTO tarefa) {
+	public ResponseEntity<String> cadastraTarefa(@RequestBody TarefaDTO tarefa) {
+		if (tarefa.titulo() == null && tarefa.descricao() == null) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Informações insuficiente, favor preencher mais algum campo");
+		}
 		cont++;
 		Tarefa tarefaAtual = new Tarefa(cont, tarefa.titulo(), tarefa.descricao(), tarefa.completa());
 		tarefas.add(tarefaAtual);
+		return ResponseEntity.ok("Tarefa registrada com sucesso");
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Tarefa> putTarefa(@PathVariable Long id, @RequestBody TarefaDTO tarefaDTO) {
+		Optional<Tarefa> opTarefa = tarefas.stream().filter(t -> t.getId().equals(id)).findFirst();
+		if (opTarefa.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		Tarefa tarefaModificada = opTarefa.get();
+		if (tarefaDTO.titulo() != null) {
+			tarefaModificada.setTitulo(tarefaDTO.titulo());
+		}
+		if (tarefaDTO.descricao() != null) {
+			tarefaModificada.setDescricao(tarefaDTO.descricao());
+		}
+		if (tarefaDTO.completa() != null) {
+			tarefaModificada.setCompleta(tarefaDTO.completa());
+		}
+		return ResponseEntity.ok(tarefaModificada);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteTarefa(@PathVariable Long id) {
+		Optional<Tarefa> opTarefa = tarefas.stream().filter(t -> t.getId().equals(id)).findFirst();
+		if (opTarefa.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ID "+ id +" não encontrado");
+		}
+		tarefas.remove(opTarefa.get());
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/jv/triersistemas/atividades/model/Tarefa.java
+++ src/main/java/jv/triersistemas/atividades/model/Tarefa.java
@@ -3,10 +3,12 @@ package jv.triersistemas.atividades.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Setter
 public class Tarefa {
 	private Long id;
 	private String titulo;
